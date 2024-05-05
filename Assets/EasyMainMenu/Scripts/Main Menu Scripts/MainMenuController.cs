98204c88e78def832427f78bf625852df192482c
Dedsecsquad <153934064+Dedsecsquad@users.noreply.github.com>
2024-05-05 19:59:53 +0300

    переход на хаб

--- Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/MainMenuController.cs
@@ -129,11 +129,9 @@ public class MainMenuController : MonoBehaviour {
 
     public void newGame()
     {
-        if (!string.IsNullOrEmpty(newGameSceneName))
-            SceneManager.LoadScene(newGameSceneName);
-        else
-            Debug.Log("Please write a scene name in the 'newGameSceneName' field of the Main Menu Script and don't forget to " +
-                "add that scene in the Build Settings!");
+        
+            SceneManager.LoadScene(1);
+        
     }
     #endregion
 
