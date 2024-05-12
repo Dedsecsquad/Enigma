df9aa9fb119cf711eae9b19af269f96d323d9a7c
DimaLimon77 <158479537+DimaLimon77@users.noreply.github.com>
2024-05-12 18:44:02 +0300

    на жопа

---
+++ Assets/Portal.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            int index = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(index + 1);
+        }
+    }
+}
