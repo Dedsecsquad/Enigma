0c6f5704fbd592f6efdf0724d82b9b37061855a4
coychance <159535207+coychance@users.noreply.github.com>
2024-05-19 21:21:08 +0300

    CutScene

---
+++ Assets/cut.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.Events;
+
+public class cut : MonoBehaviour
+{
+    public VideoPlayer video;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<VideoPlayer>().loopPointReached += videofinished;
+
+    }
+
+    // Update is called once per frame
+    void videofinished(VideoPlayer VP)
+    { 
+        gameObject.SetActive (false); 
+    }
+}
