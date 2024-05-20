4035825f1989452bbdf16fdf4cfdbbb06e417662
coychance <159535207+coychance@users.noreply.github.com>
2024-05-20 23:59:27 +0300

    Движ платформа
    Оно двигается

---
+++ Assets/Stickyplatform.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stickyplatform : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(transform);
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            collision.gameObject.transform.SetParent(null);
+        }
+    }
+}
