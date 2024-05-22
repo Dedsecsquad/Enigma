a18c1eba33940279d901992f3223d7b262ef6c3f
coychance <159535207+coychance@users.noreply.github.com>
2024-05-22 13:05:29 +0300

    Ну как-то так

---
+++ Assets/Assets start/Physic mat/cube/rotatecube.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatecube : MonoBehaviour
+{
+   
+
+
+    void Update()
+    {
+        transform.Rotate(10f * Time.deltaTime, 0f , 0F, Space.Self );
+    }
+}
