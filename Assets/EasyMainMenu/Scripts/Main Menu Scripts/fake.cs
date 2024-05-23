3ec1f2aa0c3e7c9bd2d6be729e7a042f2275703a
Dedsecsquad <153934064+Dedsecsquad@users.noreply.github.com>
2024-05-23 23:08:15 +0300

    fake quit

---
+++ Assets/EasyMainMenu/Scripts/Main Menu Scripts/fake.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class fake : MonoBehaviour
+{
+    public void newGame()
+    {
+
+        SceneManager.LoadScene(1);
+
+    }
+}
